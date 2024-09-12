389d1303b0a50e79cbf8a473d45fb38327b2ba80
Specify your username <Your email address>
2024-09-12 12:28:18 +0200

    change

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import {getUsers, getUser, insertUser, deleteUser, updateUser,loginUser} from '../controller/userController.js'
 import { registerUser } from '../controller/userController.js'
-
+import { authMiddleware } from '../middleware/authenticate.js'
 const router = express.Router()
 
 router.post('/register', registerUser);
@@ -13,7 +13,7 @@ router.
 
 router.
     route('/:userID')
-        .get(getUser)
+        .get(authMiddleware,getUser)
         .delete(deleteUser)
         .patch(updateUser)
 

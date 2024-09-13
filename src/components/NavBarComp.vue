3aac934079bec8718730a7f4fdb2efe87fe5f531
Specify your username <Your email address>
2024-09-13 16:15:28 +0200

    admin

--- src/components/NavBarComp.vue
+++ src/components/NavBarComp.vue
@@ -9,7 +9,7 @@
           <li><router-link to="/about">About</router-link></li>
           <li><router-link to="/products">Book A Cinema</router-link></li>
           <li><router-link to="/contact">Contact</router-link></li>
-          <li v-if="isAdmin"><router-link to="/admin">Admin</router-link></li>
+          <li><router-link to="/admin">admin</router-link></li>
           <li><router-link to="/signup"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
   <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
 </svg></router-link></li>
@@ -27,14 +27,6 @@
   
     <router-view />
   </template>
-  <script>
-  import { mapGetters } from 'vuex';
-
-  export default {
-  computed: {
-    ...mapGetters(['isAdmin']),
-  },
-};
-</script>
+  
   
     
